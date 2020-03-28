abd1acd8a9433af186fcb0502a1aa25fa86bb0f1
d116626 <d116626@gmail.com>
2020-03-27 23:53:39 -0300

    all

--- scripts/vis.py
+++ scripts/vis.py
@@ -16,7 +16,7 @@ def show_colors(scaled_colours):
 #     return fig
 
 
-def plot_simulation(final, columns, colors, perc_leitos=1, perc_ventiladores=1):
+def plot_simulation(final, columns, colors, config, leitos=1, perc_ventiladores=1):
     
     wid = 7
     marker_size = 5
@@ -28,6 +28,7 @@ def plot_simulation(final, columns, colors, perc_leitos=1, perc_ventiladores=1):
     
     
     data = []
+    y_list = []
     i=0
     for col in columns.keys():
         trace = go.Scatter(
@@ -44,7 +45,7 @@ def plot_simulation(final, columns, colors, perc_leitos=1, perc_ventiladores=1):
                 trace = go.Scatter(
                     name=columns[col],
                     x=final['days'], 
-                    y=round(final[col] * perc_leitos,0),
+                    y=[leitos for i in range(len(final['days']))],
                     line=dict(color=colors[i], width=3),
                     # line=dict(width=wid),
                     mode='lines+markers',
@@ -63,8 +64,10 @@ def plot_simulation(final, columns, colors, perc_leitos=1, perc_ventiladores=1):
                     hoverlabel=dict(namelength=-1, font=dict(size=18))   
                     )
         data.append(trace)
+        
+        y_list.append(max(final[col]))
+        
         i+=1
-
 
 
 
@@ -126,13 +129,46 @@ def plot_simulation(final, columns, colors, perc_leitos=1, perc_ventiladores=1):
             size=18,
         ),
           
-        hovermode = "x", 
+        hovermode = "x",
 
     )
 
+    y_max = max(y_list)
 
 
 
     fig = go.Figure(data=data, layout=layout)
+    
+    t0   = 0 
+    r_t0 = config['scenarios']['nothing']['R0']['upper_bound']
+    
+    t1   = config['scenarios']['isolation']['dates'][0]
+    r_t1 = config['scenarios']['isolation']['R0']['upper_bound']
+    
+    t2   = config['scenarios']['lockdown']['dates'][0]
+    r_t2 = config['scenarios']['lockdown']['R0']['upper_bound']
+    
+    uti_percent = config['model_parameters']['seir']['i3_percentage']['upper_bound']
+    
+    x_max = max(final['days'])
+    
+    fig.add_annotation(
+        showarrow=False,
+        x=x_max*0.9,
+        y=y_max*0.95,
+        xref="x",
+        yref="y",
+        align="left",
+        text="<b>\
+            t0  = {} dias - R0 = {}<br>\
+            t1  = {} dias -  R0 = {}<br>\
+            t1  = {} dias -  R0 = {}        <br>\
+            UTI = {}% dos Infectados \
+            <b>".format(t0,r_t0,
+                        t1,r_t1,
+                        t2,r_t2,
+                        uti_percent),
+        
+        )
 
     return fig
