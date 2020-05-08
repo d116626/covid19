1cf94e42509db135b1c30d2091c4477aa62e6c63
d116626 <d116626@gmail.com>
2020-05-08 19:41:19 -0300

    all

--- notebooks/paths.py
+++ notebooks/paths.py
@@ -1,9 +1,4 @@
 import os
 import sys
 sys.path.insert(0, '../')
-sys.path.insert(0, '../tools')
-import os
-import sys
-sys.path.insert(0, '../')
 sys.path.insert(0, '../scripts')
-from config import *
