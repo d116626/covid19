6c68e1b4987352ba7d027c6123187941c8aea568
d116626 <d116626@gmail.com>
2020-05-29 00:40:13 -0300

    all

---
+++ scripts/vis_simulaCovid.py
@@ -0,0 +1,106 @@
+# matplotlib
+from matplotlib import pyplot as plt
+from matplotlib.dates import date2num, num2date
+from matplotlib import dates as mdates
+from matplotlib import ticker
+from matplotlib.colors import ListedColormap
+from matplotlib.patches import Patch
+
+# scipy specifics
+from scipy import stats as sps
+from scipy.stats import dirichlet
+from scipy.interpolate import interp1d
+
+# GAMs and sklearn
+from sklearn.utils import resample
+
+import pandas as pd
+import numpy as np
+
+
+def plot_rt(result, ax, state_name):
+    
+    """
+    Function to plot Rt
+
+    Arguments
+    ----------
+    result: expected value and HDI of posterior
+
+    ax: matplotlib axes 
+
+    state_name: state to be considered
+
+    See also
+    ----------
+    This code is heavily based on Realtime R0
+    by Kevin Systrom
+    https://github.com/k-sys/covid-19/blob/master/Realtime%20R0.ipynb
+    """
+#     result.index = result['last_updated']
+    result.index = pd.to_datetime(result['last_updated'])
+
+    ax.set_title(f"{state_name}")
+    
+    # Colors
+    ABOVE = [1,0,0]
+    MIDDLE = [1,1,1]
+    BELOW = [0,0,0]
+    cmap = ListedColormap(np.r_[
+        np.linspace(BELOW,MIDDLE,25),
+        np.linspace(MIDDLE,ABOVE,25)
+    ])
+    color_mapped = lambda y: np.clip(y, .5, 1.5)-.5
+    
+    index = result['Rt_most_likely'].index.get_level_values('last_updated')
+    values = result['Rt_most_likely'].values
+    
+    # Plot dots and line
+    ax.plot(index, values, c='k', zorder=1, alpha=.25)
+    ax.scatter(index,
+               values,
+               s=40,
+               lw=.5,
+               c=cmap(color_mapped(values)),
+               edgecolors='k', zorder=2)
+    
+    # Aesthetically, extrapolate credible interval by 1 day either side
+    lowfn = interp1d(date2num(index),
+                     result['Rt_low_95'].values,
+                     bounds_error=False,
+                     fill_value='extrapolate')
+    
+    highfn = interp1d(date2num(index),
+                      result['Rt_high_95'].values,
+                      bounds_error=False,
+                      fill_value='extrapolate')
+    
+    extended = pd.date_range(start=pd.Timestamp('2020-03-01'),
+                             end=index[-1]+pd.Timedelta(days=1))
+    
+    ax.fill_between(extended,
+                    lowfn(date2num(extended)),
+                    highfn(date2num(extended)),
+                    color='k',
+                    alpha=.1,
+                    lw=0,
+                    zorder=3)
+
+    ax.axhline(1.0, c='k', lw=1, label='$R_t=1.0$', alpha=.25);
+    
+    # Formatting
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+    ax.xaxis.set_minor_locator(mdates.DayLocator())
+    
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.1f}"))
+    ax.yaxis.tick_right()
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.margins(0)
+    ax.grid(which='major', axis='y', c='k', alpha=.1, zorder=-2)
+    ax.margins(0)
+    ax.set_ylim(0.0, 5.0)
+    ax.set_xlim(pd.Timestamp('2020-03-01'), result.index.get_level_values('last_updated')[-1]+pd.Timedelta(days=1)) 
